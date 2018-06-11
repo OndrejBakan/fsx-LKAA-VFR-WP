33d2150a56f631cd370f2e5c7606010f96ef9a52
Ondrej Bakan <ondrej@bakan.cz>
2018-06-11 16:08:49 +0200

    Updated to AIRAC 1806 (no actual changes in waypoints).

--- generate.bat
+++ generate.bat
@@ -1,19 +1,21 @@
 @ECHO OFF
 
+CHCP 65001 > NUL
+
 SETLOCAL ENABLEEXTENSIONS
 
-@REM set path to your bglcomp.exe
+REM set path to your bglcomp.exe
 SET path_bglcomp=c:\Program Files (x86)\Lockheed Martin\Prepar3D SDK 1.4.4747.0\Environment Kit\BGL Compiler SDK\bglcomp.exe
 
-@REM generate BGL files from XML files
-ECHO "Generuji BGL soubory."
+REM Generate BGL files from XML files.
+ECHO LKAA-VFR-WP: Generuji BGL soubory.
 FOR %%f IN (%~dp0src\*.xml) DO "%path_bglcomp%" "%%~ff" > NUL
 
-@REM Create LKAA-VFR-WP\Scenery, if it does not exist.
-md "%~dp0LKAA-VFR-WP\Scenery" 2> NUL
+REM Create LKAA-VFR-WP\scenery, if it does not exist.
+md "%~dp0LKAA-VFR-WP\scenery" 2> NUL
 
-@REM move BGL files to LKAA-VFR-WP\Scenery
-ECHO "Kop¡ruji vygenerovan‚ BGL soubory do LKAA-VFR-WP\Scenery."
-move "%~dp0src\*.bgl" "%~dp0LKAA-VFR-WP\Scenery" > NUL
+REM Move BGL files to LKAA-VFR-WP\scenery.
+ECHO LKAA-VFR-WP: KopÃ­ruji vygenerovanÃ© BGL soubory do LKAA-VFR-WP\scenery.
+move "%~dp0src\*.bgl" "%~dp0LKAA-VFR-WP\scenery" > NUL
 
-ECHO "Hotovo."
+ECHO LKAA-VFR-WP: Hotovo.
